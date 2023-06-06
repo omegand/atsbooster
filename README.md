25e516cf4bf754e053834f207c1063af2afff600
Šarūnas <sarunyss@gmail.com>
2023-06-06 19:05:56 +0300

    changes

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 
 ## About <a name = "about"></a>
 
-Increases your fake attackspeed with a multiplier
+Increases your fake attackspeed with a multiplier, works way better than a base number.
+Obviously doesn't work on all the time, depends on SP used, how prone to ghosting the class is, your ping etc...
 
 ## Usage <a name = "usage"></a> 
 
--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 module.exports = function gofast(mod) {
-	
-	const SpeedDivider = [120, 100, 110, 90, 110, 120, 105, 105, 100, 100, 100, 100, 100];
+	command.add('gofast', (arg) => {
+		switch (arg) {
+			case 'multiplier'
+			case 'reload':
+				delete require.cache[require.resolve('./config')];
+				({ settings, individualEnabled, classBaseAttackSpeeds } = require('./config'));
+				command.message('Reloaded settings.');
+				break;
+			case 'help':
+				const readmePath = path.join(__dirname, 'README.md');
+				fs.readFile(readmePath, 'utf8', (err, data) => {
+					if (err) {
+						command.message('Wtf where did you put the readme');
+						console.log(err);
+					} else {
+						command.message(data);
+					}
+				});
+				break;
+			default:
+				enabled = !enabled;
+				command.message('You are' + (enabled ? '' : 'Not') + 'going fast');
+				break;
+		}
+	});
+	//import settings from config.js
+	let { settings, individualEnabled, classBaseAttackSpeeds } = require('./config');
+
 	let totalaspeed = 0;
 	let job;
-	let multiplier = 1.1;
-	
+	let multiplier = settings.multiplier;
+
 	mod.hook('S_LOGIN', 14, { order: -Infinity, filter: { fake: null } }, (event) => {
-        job = (event.templateId - 10101) % 100;
+		job = (event.templateId - 10101) % 100;
 	});
 
 	mod.hook('S_PLAYER_STAT_UPDATE', 14, { order: -Infinity, filter: { fake: null } }, (event) => {
+		if (!individualEnabled[job] || !settings.enabled) return;
 		totalaspeed = event.attackSpeed + event.attackSpeedBonus;
 		event.attackSpeed = SpeedDivider[job];
-		event.attackSpeedBonus = (totalaspeed - SpeedDivider[job]) * multiplier;
+		event.attackSpeedBonus = (totalaspeed - classBaseAttackSpeeds[job]) * multiplier;
 		return true
 	})
+
+
 }	
